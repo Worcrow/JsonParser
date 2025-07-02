1c84675bfc128ba1373bf17936da54f4000672dd
trool maroc <troolmaroc1@gmail.com>
2025-07-02 23:03:02 +0100

    update parser.py

--- src/parser.py
+++ src/parser.py
@@ -10,6 +10,8 @@ class Parser:
     def __init__(self, tokens):
         self.tokens = tokens
         self.ind = 0
+        self.values = (JsonTokenType.STRING, JsonTokenType.NUMBER, JsonTokenType.NULL,\
+                       JsonTokenType.BOOLEAN)
 
     def current(self):
         return self.tokens[self.ind]
@@ -22,34 +24,47 @@ class Parser:
         token = self.current()
         if token.type == expectedTokenType:
             self.ind += 1
+            self.skipSpace()
             return token
-
+        print(token.__repr__())
         raise Exception(f"Invalid syntax line: {token.position[0]} column: {token.position[1]}\nExepected: {expectedTokenType}, Got: {token.type}")
 
     def parse(self):
-       return self.parseValue()
+        root = AST(None, None, None)
+        self.parseValue(root)
+        return root
 
-    def parseValue(self):
+    def parseValue(self, root):
         token = self.current()
         if token.type == JsonStructuredTypeSymbol.BEGINOBJECT:
             self.advance(JsonStructuredTypeSymbol.BEGINOBJECT)
-            self.skipSpace()
             if self.current().type == JsonStructuredTypeSymbol.ENDOBJECT:
                 return AST(Token(JsonTokenType.EMPTYOBJECT, '', token.position))
             root = AST(token, None, None)
             left = AST(self.advance(JsonTokenType.STRING), None, None)
             self.advance(JsonStructuredTypeSymbol.NAMESEPARATOR)
             right = self.parseValue()
-            self.advance(JsonStructuredTypeSymbol.VALUESEPARATOR)
             root.left = left
             root.right = right
-
+            if self.current().type == JsonStructuredTypeSymbol.ENDOBJECT:
+                return root
+            self.advance(JsonStructuredTypeSymbol.VALUESEPARATOR)
             return root
 
+        elif token.type in self.values:
+            self.ind += 1
+            self.skipSpace()
+            return AST(token, None, None)
 
-lex = processFile("/Users/oel-asri/Kingsave/JsonParser/unit_test/validJson1.json")
+        else:
+            raise Exception(f"Invalid Syntax line: {token.position[0]} column: {token.position[1]}")
+        
+        
+        
+
+lex = processFile("/Users/mac/JsonParser/unit_test/validJson1.json")
 
 parser = Parser(lex.tokenStream.Token)
-parser.parse()
+ast = parser.parse()
 
-print(parser.root.node)
+print(ast.right.right)
